5b50560d645fc9c14f34f113697a2c3b6f984114
gilby547 <algilby547@gmail.com>
2025-04-06 17:10:22 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-
+    int num, sum;
+    sum = 0;
+    cout << "Enter a positive integer: ";
+    cin >> num;
+    for (int i = 1; i <= num; ++i) {
+        sum += i;
+    }
+    cout << "sum =" << sum << endl;
     return 0;
 }
